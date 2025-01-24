fd45edda03efd490c2b980b682888fba3b7c1319
taha <muhammadtaha1241@gmail.com>
2025-01-24 22:23:19 +0500

    the best version

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,4 @@
-#include <WiFi.h>        // Use WiFi.h for ESP32
-#include <WebServer.h>   // Use WebServer.h for ESP32
-#include <AutoConnect.h> // AutoConnect library for ESP32
+// AutoConnect library for ESP32
 #include <auto_connect.h>
 
 void setup()
@@ -8,17 +6,7 @@ void setup()
   Serial.begin(115200);
   delay(1000);
   esp_auto.init();
-  //  Config.autoReconnect = true;    //
-  //  Config.homeUri = "/home";
-  // Config.portalTimeout = 1000000; //
-  // Portal.config(Config);
-  // Aux1.add(customtext);
-  // Config.autoReconnect = true;///
-  // Portal.config(Config);
-  // Aux1.add(input);
-  // Aux1.add(button);
 
-  // Portal.join({Aux1, Aux2});
 }
 // Server.send();//
 void loop()
